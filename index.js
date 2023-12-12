a4abc08513e8b847fb61549bb2369afee6516a52
Villeda200 <99304779+Villeda200@users.noreply.github.com>
2023-12-12 16:43:40 -0500

    added index.js

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+const bodyParser = require('body-parser')
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
 const port = 3000;
+
+// this allows us to use all files in the public folder
+app.use(express.static(__dirname + '/public'))
+
+// GET, which serves the index.html page
+app.get("/", (req, res) => {
+  res.sendFile("public/html/index.html", { root: __dirname })
+})
+
+// GET, which serves the about.html page
+app.get("/about", (req, res) => {
+  res.sendFile("public/html/about.html", { root: __dirname })
+})
+
+// GET, which serves the catalog.html page
+app.get("/catalog", (req, res) => {
+  res.sendFile("public/html/catalog.html", { root: __dirname })
+})
+
+// example GET endpoint, which returns the current date
+app.get("/current_date", (req, res) => {
+  res.send(new Date())
+})
+
+app.listen(port)
 
 // Defining API endpoints
 app.get('/api/players', async (req, res) => {
