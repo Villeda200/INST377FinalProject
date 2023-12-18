b3aeb41bd8454ceb41e45d9ea50cbe05b55ddec4
Villeda200 <99304779+Villeda200@users.noreply.github.com>
2023-12-17 23:01:56 -0500

    Adding JS and API endpoints to home.html

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
 const bodyParser = require('body-parser')
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.use(bodyParser.json())
 
 // this allows us to use all files in the public folder
 app.use(express.static(__dirname + '/public'))
 
 // GET, which serves the index.html page
 app.get("/", (req, res) => {
-  res.sendFile("public/html/index.html", { root: __dirname })
+    res.sendFile("public/html/home.html", { root: __dirname })
 })
 
 // GET, which serves the about.html page
 app.get("/about", (req, res) => {
-  res.sendFile("public/html/about.html", { root: __dirname })
+    res.sendFile("public/html/about.html", { root: __dirname })
 })
 
 // GET, which serves the catalog.html page
-app.get("/catalog", (req, res) => {
-  res.sendFile("public/html/catalog.html", { root: __dirname })
+app.get("/help", (req, res) => {
+    res.sendFile("public/html/help.html", { root: __dirname })
 })
+
 
 // example GET endpoint, which returns the current date
 app.get("/current_date", (req, res) => {
   res.send(new Date())
 })
 
-app.listen(port)
-
-// Defining API endpoints
-app.get('/api/players', async (req, res) => {
-  try {
-    const response = await axios.get('https://www.balldontlie.io/api/v1/players');
-    const players = response.data;
-    res.json(players);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
